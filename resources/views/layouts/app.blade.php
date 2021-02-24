a3fc17ab8ea3c8205e6108df289abd8155e365d5
Minossi <rafaelminossi@gmail.com>
2021-02-24 07:17:29 -0300

    add files to heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel="stylesheet" type="text/css" />
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js'></script>
-    @livewireScripts
+    @livewireStyles
 </head>
 
 <body>
