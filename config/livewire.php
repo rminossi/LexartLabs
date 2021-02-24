8cc9b0088595d46ab83d8f64459ad8512f21e709
Minossi <rafaelminossi@gmail.com>
2021-02-24 06:54:52 -0300

    add files to heroku

--- config/livewire.php
+++ config/livewire.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'asset_url' => 'lexartlabs.herokuapp.com/public',
+    'asset_url' => null,
 
     /*
     |--------------------------------------------------------------------------
